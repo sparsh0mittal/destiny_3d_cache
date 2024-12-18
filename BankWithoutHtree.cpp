68028e1909290f8886575c4fa0cf4c578995c299
sparsh0mittal <sparsh0mittal@gmail.com>
2024-12-18 12:53:56 +0530

    Update BankWithoutHtree.cpp

--- BankWithoutHtree.cpp
+++ BankWithoutHtree.cpp
@@ -108,7 +108,7 @@ void BankWithoutHtree::Initialize(int _numRowMat, int _numColumnMat, long long _
 	numDataBitRouteToMat = blockSize;
 
 
-	if (memoryType == data) { /* Data array */
+	if (memoryType == MemoryType::data) { /* Data array */
 		numDataBitRouteToMat = blockSize / numActiveMatPerColumn / numActiveMatPerRow;
 		if (numRowPerSet > associativity) {
 			/* There is no enough ways to distribute into multiple rows */
@@ -190,7 +190,7 @@ void BankWithoutHtree::Initialize(int _numRowMat, int _numColumnMat, long long _
 		}
 
 		int numSenseAmp;
-		if (memoryType == data)
+		if (memoryType == MemoryType::data)
 			numSenseAmp = blockSize;
 		else
 			numSenseAmp = blockSize * associativity;
@@ -257,7 +257,7 @@ void BankWithoutHtree::CalculateArea() {
 		}
 
 		/* Determine if the aspect ratio meets the constraint */
-		if (memoryType == data)
+		if (memoryType == MemoryType::data)
 			if (height / width > CONSTRAINT_ASPECT_RATIO_BANK || width / height > CONSTRAINT_ASPECT_RATIO_BANK) {
 				/* illegal */
 				invalid = true;
